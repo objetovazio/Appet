7404ddd9e949b147f99b0c1a52ce35fd488a2a89
Jackson Willian <jacksonwilliansilvaagostinho@gmail.com>
2019-10-14 17:16:42 -0300

    UPDATE: atualiza aquivos do Angular Project

--- Aplicacoes/Appet-Workspace/src/app/app-routing.module.ts
+++ Aplicacoes/Appet-Workspace/src/app/app-routing.module.ts
@@ -3,14 +3,32 @@ import { Routes, RouterModule } from '@angular/router';
 
 import {  } from '@angular/router';
 import { IndexComponent } from './index/index.component';
-
+import { LoginComponent } from './login/login.component';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
+import { BuscarServicoComponent } from './buscar-servico/buscar-servico.component';
 
 const routes: Routes = [
   {
-    path: 'index',
+    path: '',
     component: IndexComponent,
     data: { title: 'Bem vindo ao Appet' }
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: { title: 'Bem vindo ao Appet' }
+  },
+  {
+    path: 'novo-usuario',
+    component: NovoUsuarioComponent,
+    data: { title: 'Bem vindo ao Appet' }
+  },
+  {
+    path: 'buscar-servico',
+    component: BuscarServicoComponent,
+    data: { title: 'Bem vindo ao Appet' }
   }
+
 ];
 
 @NgModule({
--- Aplicacoes/Appet-Workspace/src/app/app.module.ts
+++ Aplicacoes/Appet-Workspace/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
+import { MenuPrincipalComponent } from './menu-principal/menu-principal.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { LoginComponent } from './login/login.component';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
+import { BuscarServicoComponent } from './buscar-servico/buscar-servico.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
+    MenuPrincipalComponent,
+    RodapeComponent,
+    LoginComponent,
+    NovoUsuarioComponent,
+    BuscarServicoComponent,
   ],
   imports: [
     BrowserModule,
