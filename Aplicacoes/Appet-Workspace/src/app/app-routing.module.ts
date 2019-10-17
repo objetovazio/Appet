62b1fa2f0b4b3ebae2eb6c80d5ec1f692950dc09
André Martins <objetovazio@gmail.com>
2019-10-16 22:16:55 -0300

    index to home

--- Aplicacoes/Appet-Workspace/src/app/app-routing.module.ts
+++ Aplicacoes/Appet-Workspace/src/app/app-routing.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {  } from '@angular/router';
-import { IndexComponent } from './index/index.component';
 import { LoginComponent } from './login/login.component';
 import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 import { BuscarServicoComponent } from './buscar-servico/buscar-servico.component';
 import { MeuPerfilComponent} from './meu-perfil/meu-perfil.component';
 import { EditarPerfilComponent} from './editar-perfil/editar-perfil.component';
 import { AgendaComponent } from './agenda/agenda.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: IndexComponent,
+    component: HomeComponent,
     data: { title: 'Bem vindo ao Appet' }
   },
   {
@@ -46,8 +46,6 @@ const routes: Routes = [
     component: AgendaComponent,
     data: { title: 'Bem vindo ao Appet' }
   }
-  
-
 ];
 
 @NgModule({
