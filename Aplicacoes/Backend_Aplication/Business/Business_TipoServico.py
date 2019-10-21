ad587a7f01cf0e3cb3ef5ee4c5912999cb8ac3b0
Elimar Macena <elimar.macena@gmail.com>
2019-10-21 09:09:35 -0300

    ADD: busniess tipo servico

---
+++ Aplicacoes/Backend_Aplication/Business/Business_TipoServico.py
@@ -0,0 +1,46 @@
+
+# used to resolve the path problem
+import sys
+from os.path import dirname, abspath
+diretorio = dirname(dirname(abspath(__file__)))
+sys.path.append(diretorio)
+# ---------------------------------
+import Model.TipoServico as TS
+
+def createTypeService(ts_name:str):
+    new_typeService = TS.TipoServico(nome_tipo = ts_name)
+    try:
+        new_typeService.save()
+        return True
+    except Exception as err:
+        print(err)
+        return False
+
+def updateTypeService(ts_data:dict):
+    old_ts = TS.TipoServico.get_by_id(ts_data['id_ts'])
+    old_ts.nome_tipo = ts_data['nome_ts']
+    try:
+        old_ts.save()
+        return True
+    except Exception as err:
+        print(err)
+        return False
+
+def findTypeService(data_query:dict):
+    result_query = None
+    have_id = data_query['id_ts'] != '' and data_query['id_ts'] != None
+    if(have_id):
+        result_query = TS.TipoServico.get_by_id(data_query['id_ts'])
+    else:
+        result_query = TS.TipoServico.select().where(TS.TipoServico.contains(data_query['nome_ts']))
+    final_result = []
+    for result in result_query:
+        final_result.append(_makeDic(result))
+    return final_result
+
+def _makeDic(ts_data):
+    ts_dic = {
+        'id':ts_data.id_tipo,
+        'nome': ts_data.nome_tipo
+    }
+    return ts_dic
