3e7236bd9fd628c1a08cd9d0d228c52a244f1d93
André Martins <objetovazio@gmail.com>
2019-11-02 15:29:42 -0300

    Changed user to object

--- Aplicacoes/Frontend_jQUERY/assets/js/usuario.js
+++ Aplicacoes/Frontend_jQUERY/assets/js/usuario.js
@@ -1,122 +1,123 @@
+class Usuario {
+	static Novo(usuarioData) {
+
+		var data_request = {
+			userId: "",
+			nomeUser: $("#nome").val(),
+			emailUser: $("#email").val(),
+			senha: $("#senha").val(),
+			sobre: ""
+		};
+
+		if (Usuario.validaUsuario(data_request) == true) {
+
+			$.post(rota_user, data_request, function () {
+			}).done(function () {
+
+				var texto = "Cadastro realizado!";
+				mensagem(texto, "Sucesso", 2000);
+
+				$("#nome").val("");
+				$("#email").val("");
+				$("#senha").val("");
 
 
+			}).fail(function (msg) {
 
-function adicionaUsuario (){
-
-	var data_request = {
-		userId:"",
-		nomeUser:$("#nome").val(),
-		emailUser:$("#email").val(),
-		senha:$("#senha").val(),
-		sobre: ""
-	};
-
-	if (validaUsuario( data_request ) == true){
-
-		$.post(rota_user, data_request, function(){
-		}).done( function (){
-
-			var texto = "Cadastro realizado!";
-			mensagem(texto, "Sucesso", 2000);
-
-			$("#nome").val("");
-			$("#email").val("");
-			$("#senha").val("");
-
-
-		}).fail( function (msg) {
-
-			var texto = "Falha ao realizar cadastro! Status: " + msg.status + " | Motivo: " + msg.responseText ;
-			mensagem(texto, "Erro",5000);
-		});
+				var texto = "Falha ao realizar cadastro! Status: " + msg.status + " | Motivo: " + msg.responseText;
+				mensagem(texto, "Erro", 5000);
+			});
+		}
 	}
-}
 
+	static pegaUsuario() {
 
-function pegaUsuario (){
+		var data_request = {
+			userId: "",
+			nomeUser: "",
+			emailUser: "carla@gmail.com",
+			senha: "",
+			sobre: ""
+		};
 
-	var data_request = {
-		userId: "",
-		nomeUser:"",
-		emailUser: "carla@gmail.com",
-		senha:"",
-		sobre:""
-	};
-	
-	$.get(rota_user, data_request, function(){
-	}).done( function (dados){
+		$.get(rota_user, data_request, function () {
+		}).done(function (dados) {
 
-		// convert a strgin em objeto e ao mesmo tempo
-		// acessa a chave data no indice 0
-		var usuario = JSON.parse(dados).data[0];
+			// convert a strgin em objeto e ao mesmo tempo
+			// acessa a chave data no indice 0
+			var usuario = JSON.parse(dados).data[0];
 
-		$("#id").val(usuario["user_id"]);
-		$("#nome").val(usuario["name"]);
-		$("#email").val(usuario["email"]);
-		$("#senha").val(usuario["password"]);
-		$("#sobre").val(usuario["about"]);
-	
-	}).fail( function (msg) {
+			$("#id").val(usuario["user_id"]);
+			$("#nome").val(usuario["name"]);
+			$("#email").val(usuario["email"]);
+			$("#senha").val(usuario["password"]);
+			$("#sobre").val(usuario["about"]);
 
-		var texto = "Falha ao tentar recuperar os dados do servidor! Status: " + msg.status + " | Motivo: " + msg.responseText ;
-		mensagem(texto, "Erro",5000);
-	});
+		}).fail(function (msg) {
 
-}
-
-
-function atualizaUsuario(){
-
-	var data_request = {
-		userId:$("#id").val(),
-		nomeUser:$("#nome").val(),
-		emailUser:$("#email").val(),
-		senha:$("#senha").val(),
-		sobre:$("#sobre").val()
-	};
-
-	console.log(data_request);
-
-	if (validaUsuario( data_request ) == true){
-
-		$.post(rota_user, data_request, function(){
-		}).done( function (){
-
-			var texto = "Atualização realizada!";
-			mensagem(texto, "Sucesso", 2000);
-
-		}).fail( function (msg) {
-
-			var texto = "Falha ao realizar a atualização! Status: " + msg.status + " | Motivo: " + msg.responseText ;
-			mensagem(texto, "Erro",5000);
+			var texto = "Falha ao tentar recuperar os dados do servidor! Status: " + msg.status + " | Motivo: " + msg.responseText;
+			mensagem(texto, "Erro", 5000);
 		});
+
 	}
-}
 
-                                                               
-function validaUsuario( usuario ){
-	var msg = "";
-	try { 
-		if (usuario.nomeUser.length == 0){
-			 msg = "Preencha o campo nome";
-		 	 throw msg;}
-		if (usuario.emailUser.length == 0) {
-			msg = "Preencha o campo e-mail";
-			throw  msg;}
-		// verifica '@' e '.' 
-		var indice = Number(usuario.emailUser.indexOf("@"));
-		if ( !(indice > 0) || !(usuario.emailUser.includes(".", indice)) ) {
-			msg = "Utilize um e-mail válido";
-			throw msg;}
-		if (usuario.senha.length == 0) {
-			msg = "Preencha o campo senha"
-			throw msg;}
-		return true;
-	}catch(err) {
-		mensagem(msg, "Atencao",2000);
-    	return false;
-  	}
+	static atualizaUsuario() {
 
+		var data_request = {
+			userId: $("#id").val(),
+			nomeUser: $("#nome").val(),
+			emailUser: $("#email").val(),
+			senha: $("#senha").val(),
+			sobre: $("#sobre").val()
+		};
+
+		console.log(data_request);
+
+		if (Usuario.validaUsuario(data_request) == true) {
+
+			$.post(rota_user, data_request, function () {
+			}).done(function () {
+
+				var texto = "Atualização realizada!";
+				mensagem(texto, "Sucesso", 2000);
+
+			}).fail(function (msg) {
+
+				var texto = "Falha ao realizar a atualização! Status: " + msg.status + " | Motivo: " + msg.responseText;
+				mensagem(texto, "Erro", 5000);
+			});
+		}
+	}
+
+
+	static validaUsuario(usuario) {
+		var msg = "";
+		try {
+			if (usuario.nomeUser.length == 0) {
+				msg = "Preencha o campo nome";
+				throw msg;
+			}
+			if (usuario.emailUser.length == 0) {
+				msg = "Preencha o campo e-mail";
+				throw msg;
+			}
+			// verifica '@' e '.' 
+			var indice = Number(usuario.emailUser.indexOf("@"));
+			if (!(indice > 0) || !(usuario.emailUser.includes(".", indice))) {
+				msg = "Utilize um e-mail válido";
+				throw msg;
+			}
+			if (usuario.senha.length == 0) {
+				msg = "Preencha o campo senha"
+				throw msg;
+			}
+			return true;
+		} catch (err) {
+			mensagem(msg, "Atencao", 2000);
+			return false;
+		}
+
+	}
 }
 
 // function getUsuario (){
@@ -130,7 +131,7 @@ function validaUsuario( usuario ){
 // 	};
 
 // 	$.get(url_request, data_request,function(result){
-		
+
 // 		alert(result);
 
 // 	}
