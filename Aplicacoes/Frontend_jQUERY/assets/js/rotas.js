fd349e4115dfbe9ae570c0d3cd35de7545b17787
Jackson Willian <jacksonwilliansilvaagostinho@gmail.com>
2019-11-02 19:56:31 -0300

    UPDATE: Divide  página em Página Periodo e Página Horário

--- Aplicacoes/Frontend_jQUERY/assets/js/rotas.js
+++ Aplicacoes/Frontend_jQUERY/assets/js/rotas.js
@@ -1,5 +1,6 @@
 const rota_raiz = "http://127.0.0.1:5000";
 const rota_user = rota_raiz+"/user";
-const rota_periodo_atividade = rota_raiz+"/atividade";
+const rota_periodo_atividade = rota_raiz+"/AtivityTime";
+const rota_horario_Servico = rota_raiz+"/ServiceSchedule";
 const rota_servico = rota_raiz+"/Service";
 const rota_tipo_servico = rota_raiz+"/TypeService";
