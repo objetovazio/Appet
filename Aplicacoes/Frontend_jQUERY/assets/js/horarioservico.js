f9c0d8f2f164a630574f040a2ff745614d356060
Lavinia Corteletti <laviniacort@gmail.com>
2019-11-04 20:17:45 -0300

    UPDATE: Horario serviço
    comecinho

---
+++ Aplicacoes/Frontend_jQUERY/assets/js/horarioservico.js
@@ -0,0 +1,34 @@
+function adicionaHorarioDisp(){
+
+    //formato aaaaMMdd
+
+	raw_date_bagin = $("#dataInicial").val().split('-');
+	if ((raw_date_bagin[0].length == 4) && (raw_date_bagin[1].length == 2) && (raw_date_bagin[2].length == 2)){
+		input_date_begin = raw_date_bagin[0]+raw_date_bagin[1]+raw_date_bagin[2];
+	}else{
+		input_date_begin = "";
+	}
+
+	//formato aaaaMMdd
+	raw_date_end = $("#dataFinal").val().split('-');
+	if((raw_date_end[0].length == 4) && (raw_date_end[1].length == 2) && (raw_date_end[2].length == 2)){
+		input_date_end = raw_date_end[0]+raw_date_end[1]+raw_date_end[2];
+	}else{
+		input_date_end = "";
+    }
+
+    raw_time_begin = $("horarioInicial").val();
+    raw_time_end = $("horarioFinal").val();
+    
+
+	var data_request = {
+		beginDate:input_date_begin,
+        endDate:input_date_end,
+        beginTime:input_time_begin,
+        endTime:input_time_end,
+		ownerId:"1",
+		horarioId:$("#idHorario").val()
+    }
+};
+
+
