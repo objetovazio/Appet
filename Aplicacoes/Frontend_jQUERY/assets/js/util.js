8055b1ea51007f08edb05c4a83a5f6688a793d84
André Martins <objetovazio@gmail.com>
2019-11-05 21:55:02 -0300

    Footer and Header Templates

--- Aplicacoes/Frontend_jQUERY/assets/js/util.js
+++ Aplicacoes/Frontend_jQUERY/assets/js/util.js
@@ -2,23 +2,25 @@
 
 // Mensagem
 
-function mensagem (msg, tipo, tempo){
+function mensagem(msg, tipo, tempo) {
 
-// Define a cor da mensagem de acordo com o tipo
-var cor = ""
-if (tipo == "Sucesso"){
-  cor = "Cyan";
-}else if (tipo == "Erro"){
-  cor = "Coral";
-}else if (tipo == "Atencao"){
-  cor = "Khaki";
-}
+  // Define a cor da mensagem de acordo com o tipo
+  var cor = ""
+  if (tipo == "Sucesso") {
+    cor = "Cyan";
+  } else if (tipo == "Erro") {
+    cor = "Coral";
+  } else if (tipo == "Atencao") {
+    cor = "Khaki";
+  }
 
-$("#box-notificacao").css("background-color", cor);
-$("#box-notificacao").fadeIn("slow");
-$("#msg-notificacao").html(msg);
-setTimeout(function(){  $("#box-notificacao").fadeOut("fast");
-                        $("#msg-notificacao").html("") }, Number(tempo));
+  $("#box-notificacao").css("background-color", cor);
+  $("#box-notificacao").fadeIn("slow");
+  $("#msg-notificacao").html(msg);
+  setTimeout(function () {
+    $("#box-notificacao").fadeOut("fast");
+    $("#msg-notificacao").html("")
+  }, Number(tempo));
 }
 
 // Menu transparente
@@ -26,11 +28,11 @@ setTimeout(function(){  $("#box-notificacao").fadeOut("fast");
 jQuery(function () {
   jQuery(window).scroll(function () {
     if (jQuery(this).scrollTop() > 200) {
-     $("#menu-principal").addClass("menu-principal-transparente");
-       $("#topo").removeClass("topo-visivel");
+      $("#menu-principal").addClass("menu-principal-transparente");
+      $("#topo").removeClass("topo-visivel");
     } else {
-     $("#menu-principal").removeClass("menu-principal-transparente");
-    
+      $("#menu-principal").removeClass("menu-principal-transparente");
+
       $("#topo").addClass("topo-visivel");
     }
   });
@@ -38,8 +40,36 @@ jQuery(function () {
 
 // Função ir ao topo
 
-function paraTopo(){ 
-  
-  $('html, body').animate({scrollTop:0}, 'slow');
+function paraTopo() {
+
+  $('html, body').animate({ scrollTop: 0 }, 'slow');
 
 }
+
+function verificarRotaLogado() {
+  $.get(rota_sessao, function (logged_user) {
+    var data = JSON.parse(logged_user);
+
+    var rotas_unlogged = ['index.html', 'cadastro-servico.html', 'login.html', 'novo-usuario.html'];
+    var rotaAtual = document.location.href.match(/[^\/]+$/)[0];
+
+    if (data['success'] == false) {
+
+      if (!rotas_unlogged.includes(rotaAtual)) {
+        mensagem("Usuário deve estar logado para acessar página atual.", "Erro", 5000);
+        setTimeout(function () {
+          window.location = './index.html';
+        }, 2000);
+      }
+    }
+  });
+}
+
+
+$(function () {
+  $("#header").load("./templates/header.html");
+  $("#footer").load("./templates/footer.html");
+
+
+  verificarRotaLogado();
+})
---
+++ Aplicacoes/Frontend_jQUERY/readme.md
@@ -0,0 +1,7 @@
+# Appet - Front End
+Para rodar o front-end do Appet é necessário ter o NodeJs instalado para instalação de novos pacotes. Instale o seguinte componente:
+
+`npm install -g live-server` 
+
+Após concluir a instalação, abra o terminal no diretório do projeto front end e execute o comando:
+`live-server`
